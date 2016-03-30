9a16b76344fcb0cce8dc4abbd448084cd917c617
Yi Kai <beliefs111@gmail.com>
2016-03-30 22:23:41 +0800

    rewrite sloc calculating logic

--- js/main.js
+++ js/main.js
@@ -16,36 +16,33 @@ function insertSloc() {
     var tasks = [];
 
     $('.repo-list-name a').each(function () {
-        var self = this;
-        var repoListMeta = $(self).parent().siblings(".repo-list-meta");
-        return tasks.push(getSloc($(self).text()).then(function (lines) {
-            return repoListMeta.text(repoListMeta.text() + ", Total sloc is " + lines);
-        }, function(e){
-            return console.error(e)
-        }));
+        let self = this;
+        let repoListMeta = $(self).parent().siblings(".repo-list-meta");
+        let task = getSloc($(self).text(), 5)
+            .then(lines => repoListMeta.text(repoListMeta.text() + ", Total sloc is " + lines))
+            .catch(e=>console.error(e));
+        return tasks.push(task);
     });
+    
     return Promise.all(tasks);
 
 }
 
-function getSloc(repo) {
+function getSloc(repo, tries) {
     "use strict";
 
-    var url = "https://api.github.com/repos/" + repo + "/stats/contributors?access_token=" + github_token;
+    //Github's API returns an empty object the first time it is accessed
+    //We try five times then stop
+    if (tries === 0) {
+        return Promise.reject("Too many tries");
+    }
 
-    return fetch(url).then(function (response) {
-        return response.json();
-    }).then(function (contributors) {
-        return contributors.map(function (contributor) {
-            return contributor.weeks.reduce(function (lineCount, week) {
-                return lineCount + week.a - week.d;
-            }, 0);
-        });
-    }).then(function (lineCounts) {
-        return lineCounts.reduce(function (lineTotal, lineCount) {
-            return lineTotal + lineCount;
-        });
-    });
+    var url = "https://api.github.com/repos/" + repo + "/stats/code_frequency?access_token=" + github_token;
+
+    return fetch(url)
+        .then(x=>x.json())
+        .then(x=>x.reduce((total, changes)=>total + changes[1] + changes[2], 0))
+        .catch(err => getSloc(repo, tries - 1));
 }
 
 window.onload = load();
