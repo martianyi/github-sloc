108d3df143e71be1a9963a9cda55494a22fb246a
Yi Kai <beliefs111@gmail.com>
2016-03-30 20:26:35 +0800

    finish 0.1.0

--- js/main.js
+++ js/main.js
@@ -1,30 +1,51 @@
-function insertSloc(){
-	"use strict";
-	var tasks = [];
-	if($('.repo-list-name a')){
-		$('.repo-list-name a').each(function(){
-			let repoListMeta = $(this).parent().siblings(".repo-list-meta");
-			return tasks.push(getSloc($(this).text()).then(lines => repoListMeta.text(repoListMeta.text() + ", Total sloc:"+ lines)))
-		})
-		return Promise.all(tasks);
-	}
+"use strict";
+var github_token = "";
+
+function load() {
+    chrome.storage.sync.get('github_token', function (result) {
+        if (result.github_token != "null") github_token = result.github_token;
+        if (document.location.pathname === '/search') {
+            insertSloc();
+        }
+    })
 }
 
-function getSloc(repo){
-	"use strict";
+function insertSloc() {
+    "use strict";
 
-	var github_token = localStorage.github_token;
-	github_token = github_token?github_token:'';
+    var tasks = [];
 
-	let url = "https://api.github.com/repos/{repo}/stats/contributors?access_token="+github_token;
-	url.replace(/{repo}/, repo);
+    $('.repo-list-name a').each(function () {
+        var self = this;
+        var repoListMeta = $(self).parent().siblings(".repo-list-meta");
+        return tasks.push(getSloc($(self).text()).then(function (lines) {
+            return repoListMeta.text(repoListMeta.text() + ", Total sloc is " + lines);
+        }, function(e){
+            return console.error(e)
+        }));
+    });
+    return Promise.all(tasks);
 
-	return fetch(url)
-	.then(response => response.json())
-	.then(contributors => contributors
-	.map(contributor => contributor.weeks
-	.reduce((lineCount, week) => lineCount + week.a - week.d, 0)))
-	.then(lineCounts => lineCounts.reduce((lineTotal, lineCount) => lineTotal + lineCount))
 }
 
-window.onload = insertSloc;
+function getSloc(repo) {
+    "use strict";
+
+    var url = "https://api.github.com/repos/" + repo + "/stats/contributors?access_token=" + github_token;
+
+    return fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (contributors) {
+        return contributors.map(function (contributor) {
+            return contributor.weeks.reduce(function (lineCount, week) {
+                return lineCount + week.a - week.d;
+            }, 0);
+        });
+    }).then(function (lineCounts) {
+        return lineCounts.reduce(function (lineTotal, lineCount) {
+            return lineTotal + lineCount;
+        });
+    });
+}
+
+window.onload = load();
--- js/options.js
+++ js/options.js
@@ -1,7 +1,17 @@
-var github_token = localStorage.github_token;
-github_token = github_token?github_token:'';
-document.getElementById('github_token').value = github_token;
-document.getElementById('save').onclick = function(){
-    localStorage.github_token = document.getElementById('github_token').value;
-    alert('保存成功。');
-}
+var github_token = "";
+chrome.storage.sync.get('github_token', function (result) {
+    if (result.github_token != "null") github_token = result.github_token;
+    document.getElementById('github_token').value = github_token;
+    document.getElementById('save').onclick = function () {
+        if (!document.getElementById('github_token').value) {
+            alert('请填入github token');
+            return;
+        }
+        // Save it using the Chrome extension storage API.
+        chrome.storage.sync.set({'github_token': document.getElementById('github_token').value}, function () {
+            // Notify that we saved.
+            alert('保存成功。');
+        });
+
+    };
+});
