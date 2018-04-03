a2ab7128be29199dba86a65c7dab0f21ef55c97d
Yi Kai <beliefs111@gmail.com>
2018-04-03 12:32:41 +0800

    support topic page

--- js/injected.js
+++ js/injected.js
@@ -3,20 +3,19 @@
 var github_sloc_token
 var discoverReposObserved = false
 var miniReposObserved = false
+var topicReposObserved = false
 var observeConf = {childList: true, subtree: true}
 
 chrome.storage.sync.get('github_sloc_token', function (result) {
   if (result && result.github_sloc_token != null) github_sloc_token = result.github_sloc_token
   insertSloc()
-  $(document).on('pjax:complete', function () {
-    insertSloc()
-  })
+  $(document).on('pjax:complete', insertSloc)
 })
 
 function insertSloc () {
 
   // Project detail page
-  const $repoMeta = $('.repository-meta-content')
+  var $repoMeta = $('.repository-meta-content')
   if ($repoMeta.length !== 0) {
     $repoMeta.append('<span class="github-sloc"></span>')
     getSloc(location.pathname, 3)
@@ -25,7 +24,7 @@ function insertSloc () {
     return
   }
 
-  // Discover repositories page
+  // Discover repos page
   if (location.pathname === '/dashboard/discover') {
     var targetNode = document.getElementById('recommended-repositories-container')
     if (!discoverReposObserved) {
@@ -50,7 +49,8 @@ function insertSloc () {
   var MINI_REPO_CLASS = '.mini-repo-list-item'
   if (!miniReposObserved) {
     var yourRepos = document.getElementById('your_repos')
-    if (yourRepos) {
+    var orgYourRepos = document.getElementById('org_your_repos')
+    if (yourRepos || orgYourRepos) {
       var miniRepoCallback = function (mutations) {
         mutations.forEach(mutation => {
           mutation.addedNodes.forEach(node => {
@@ -61,15 +61,37 @@ function insertSloc () {
         })
       }
       var miniRepoObserver = new MutationObserver(miniRepoCallback)
-      miniRepoObserver.observe(yourRepos, observeConf)
+      if (yourRepos) {
+        miniRepoObserver.observe(yourRepos, observeConf)
+      }
+      if (orgYourRepos) {
+        miniRepoObserver.observe(orgYourRepos, observeConf)
+      }
       miniReposObserved = true
     }
   }
   $(MINI_REPO_CLASS).each(appendSloc)
 
-  // Explore, Collections page
+  // Explore, Collections, Topics page
   $('article h1 a').each(appendSloc)
   $('article h3 a').each(appendSloc)
+  if (!topicReposObserved) {
+    var topicNode = document.querySelector('.container-lg.topic.p-responsive')
+    if (topicNode) {
+      var topicRepoCallback = function (mutations) {
+        mutations.forEach(mutation => {
+          mutation.addedNodes.forEach(node => {
+            if (/article/i.test(node.tagName)) {
+              $(node).find('h3 a').each(appendSloc)
+            }
+          })
+        })
+      }
+      var topicRepoObserver = new MutationObserver(topicRepoCallback)
+      topicRepoObserver.observe(topicNode, observeConf)
+      topicReposObserved = true
+    }
+  }
 
   // Search, Trending page, etc
   $('.repo-list h3 a').each(appendSloc)
@@ -90,22 +112,17 @@ function appendSloc () {
 }
 
 function getSloc (repo, tries) {
-
   if (!repo) {
     return Promise.reject(new Error('No repo provided'))
   }
-
   //We try several times then stop
   if (tries === 0) {
     return Promise.reject(new Error('Too many requests'))
   }
-
   let url = 'https://api.github.com/repos' + repo + '/stats/code_frequency'
-
   if (github_sloc_token != null) {
     url += '?access_token=' + github_sloc_token
   }
-
   return fetch(url)
     .then(x => x.json())
     .then(x => x.reduce((total, changes) => total + changes[1] + changes[2], 0))
